cb349dae24cb4ccf3dd61bad5b38c307a7fb93cd
Abhishek Mohata <amohata.007@gmail.com>
2026-05-10 23:01:34 +0530

    Database

---
+++ database.js
@@ -0,0 +1,29 @@
+const {MongoClient} = require("mongodb");
+
+const url = "mongodb+srv://amohata007:password@namastenode.6jakfh0.mongodb.net/";
+
+const client = new MongoClient(url);
+
+const dbName = 'NodeJS';
+
+async function main(){
+    await client.connect();
+    console.log("Connected Sucessfully");
+    const db = client.db(dbName);
+    const collection = db.collection('Victory');
+
+    await collection.insertOne({
+        name: "Rahul",
+        age: 21
+    });
+
+    const findResult = await collection.find({}).toArray();
+    console.log(findResult);
+
+    return 'done';
+}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(()=>client.close());
