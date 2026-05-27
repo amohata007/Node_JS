f87c7d80710af64fd8655df2aa6a02879bfbe421
Abhishek Mohata <amohata.007@gmail.com>
2026-05-27 13:08:51 +0530

    Request

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,14 +3,14 @@ const { auth } = require("../middlewares/auth");
 const requestRoute = express.Router();
 
 //connection request
-requestRoute.post("/connectionRequest",auth, async (req,res)=>{
-    try{
-        const {firstName, lastName} = req.userDetail;
-        res.send(firstName+" "+lastName+" send the connection request");
+requestRoute.post("/connectionRequest", auth, async (req, res) => {
+    try {
+        const { firstName, lastName } = req.userDetail;
+        res.send(firstName + " " + lastName + " send the connection request");
     }
     catch (err) {
         res.status(400).send(err.message);
     }
 })
 
-module.exports = {requestRoute};
+module.exports = { requestRoute };
