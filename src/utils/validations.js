2112cef2c9080c5283a5e9180f3b9ff02fdbe294
Abhishek Mohata <amohata.007@gmail.com>
2026-06-05 17:35:40 +0530

    Validations

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,33 +1,33 @@
 const validator = require("validator");
 
 const validateSignUpData = (req) => {
-    const {firstName,lastName,emailId,password} = req.body;
+    const { firstName, lastName, emailId, password } = req.body;
 
-    if(!firstName || !lastName || !emailId || !password){
+    if (!firstName || !lastName || !emailId || !password) {
         throw new Error("Please enter required fields");
     }
-    else if(!validator.isEmail(emailId)){
+    else if (!validator.isEmail(emailId)) {
         throw new Error("Email is not valid");
     }
-    else if(!validator.isStrongPassword(password)){
+    else if (!validator.isStrongPassword(password)) {
         throw new Error("Password is not strong")
     }
 }
 
 const validateProfileData = (req) => {
-    const allowedUpdates = ["firstName","lastName","skills","bio","gender","age","photoUrl"];
+    const allowedUpdates = ["firstName", "lastName", "skills", "bio", "gender", "age", "photoUrl"];
     const keys = Object.keys(req.body);
-    const isAllowed = keys.every((field)=>allowedUpdates.includes(field));
-    if(!isAllowed){
+    const isAllowed = keys.every((field) => allowedUpdates.includes(field));
+    if (!isAllowed) {
         throw new Error("Invalid fields..!!");
     }
 }
 
 const validatePassword = (req) => {
-    const {newPassword, retypePassword} = req.body;
-    if(!validator.isStrongPassword(newPassword) || !validator.isStrongPassword(retypePassword)){
+    const { newPassword, retypePassword } = req.body;
+    if (!validator.isStrongPassword(newPassword) || !validator.isStrongPassword(retypePassword)) {
         throw new Error("Please enter strong password..!!")
     }
 }
 
-module.exports = {validateSignUpData, validateProfileData, validatePassword}
+module.exports = { validateSignUpData, validateProfileData, validatePassword }
