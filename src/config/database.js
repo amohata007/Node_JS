1055a79f0cc8411063c143a4e1a49150a26ce330
Abhishek Mohata <amohata.007@gmail.com>
2026-06-06 12:04:03 +0530

    DB

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const { URL_NEW } = require("../../Practice/secret");
 
-const connectDb = async()=>{
+const connectDb = async () => {
     await mongoose.connect(URL_NEW);
-} 
+}
 
-module.exports = {connectDb};
+module.exports = { connectDb };
